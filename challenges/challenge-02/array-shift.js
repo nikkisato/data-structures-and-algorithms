f513287267889dcfa83a8f07ee4edba72d9e9c10
Nikki <nikki.satopdx@gmail.com>
2019-12-06 08:57:43 -0800

    added comments

--- challenges/challenge-02/array-shift.js
+++ challenges/challenge-02/array-shift.js
@@ -4,7 +4,7 @@ function arrayShift = (array, length) => {
   const output = [];
   for(let i = 0; i < arr.length; i++) {
     const index = (strings[2]);
-};
+}};
 
-}
+//I was hoping the (strings[2]) would indicate the middle of the array?
 
