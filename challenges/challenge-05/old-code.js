febab38cb90fb1d5b600c24c3e941f9afdeaf3ed
Nikki <nikki.satopdx@gmail.com>
2019-12-10 09:24:44 -0800

    added previous code to compare

---
+++ challenges/challenge-05/old-code.js
@@ -0,0 +1,45 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+  insert(value){
+    const node = new Node(value);
+    if(this.head === null){
+      this.head = node;
+    }
+    else {
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+  includes(value){
+    let currentNode = this.head;
+    if(currentNode.value === value){
+      return true;
+    }
+    while(currentNode.next !== null){
+      if(currentNode.value === value){
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
+  toString(){
+    let str = '';
+    let currentNode = this.head;
+    str += currentNode.value;
+    while(currentNode.next !== null){
+      currentNode = currentNode.next;
+      str += ` -> ${currentNode.value.toString()}`;
+    }
+    return str;
+  }
+}
+module.exports = { LinkedList, Node };
