248278dfcaae9b256fd52794375380da314fb998
Nikki <nikki.satopdx@gmail.com>
2019-12-10 12:29:10 -0800

    passing tests

--- challenges/challenge-05/__tests__/linked-list.test.js
+++ challenges/challenge-05/__tests__/linked-list.test.js
@@ -9,56 +9,57 @@ describe('node', () => {
   it('has a value', () => { 
     expect(node.value).toEqual('testing');
   });
-    it('has a next', () => {
-      expect(node.next).toEqual(null);
-    });
+  it('has a next', () => {
+    expect(node.next).toEqual(null);
+  });
+});
+
+
+describe('linked List', () => {
+  let list;
+  
+  beforeEach(() => {
+    list = new LinkedList();
+  });
+
+  const checkMeOut = () => {
+    list.insert('check');
+    list.insert('me');
+    list.insert('out');
+  };
+
+  it('has a head', () => { 
+    expect(list.head).toEqual(null);
+  });
+  it('can insert a node to the beginning with insert', () => {
+    list.insert('first insert');
+    expect(list.head.value).toEqual('first insert');
+    list.insert('second insert');
+    expect(list.head.value).toEqual('second insert');
+  });
+
+  it('can check if list includes a value with includes', () => {
+    checkMeOut();
+
+    expect(list.includes('me')).toEqual(true);
+    expect(list.includes('out')).toEqual(true);
+    expect(list.includes('check')).toEqual(true);
+    expect(list.includes('corgi')).toEqual(false);
+  });
+
+  it('returns a string of values within the list with toString', () => {
+    checkMeOut();
+    expect(list.toString()).toEqual('out->me->check');
   });
 
 
-  describe('linked List', () => {
-    let list;
-  
-    beforeEach(() => {
-      list = new LinkedList();
-    });
-
-    const checkMeOut = () => {
-      list.insert('check');
-      list.insert('me');
-      list.insert('out');
-    }
-
-    it('has a head', () => { 
-      expect(list.head).toEqual(null);
-    });
-      it('can insert a node to the beginning with insert', () => {
-        list.insert('first insert');
-        expect(list.head.value).toEqual('first insert');
-        list.insert('second insert');
-        expect(list.head.value).toEqual('second insert');
-      });
-
-      it('can check if list includes a value with includes', () => {
-        checkMeOut();
-        expect(list.includes('me')).toEqual(true);
-        expect(list.includes('out')).toEqual(true);
-        expect(list.includes('check')).toEqual(true);
-        expect(list.includes('corgi')).toEqual(false);
-      });
-
-      it('returns a string of values within the list with toString', () => {
-        checkMeOut();
-        expect(list.toString()).toEqual('out -> me -> check');
-      });
-
-      
-    });
-
-
-
-
-
 });
+
+
+
+
+
+
 
 
 
