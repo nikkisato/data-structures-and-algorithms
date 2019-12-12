3a11700eb2d8f906ad72979b0b8d49edda648889
Nikki <nikki.satopdx@gmail.com>
2019-12-11 17:50:52 -0800

    added code

--- challenges/challenge-08/ll-merge.js
+++ challenges/challenge-08/ll-merge.js
@@ -10,7 +10,7 @@ class Node {
 class LinkedList {
   constructor(){
     this.head = null;
-    this.tail =
+    this.tail = null;
   }
 
   
@@ -80,6 +80,33 @@ class LinkedList {
       current = current.head;
     }
     return current.value;
+  }
+
+  mergeTwoLists(l1, l2) {
+    var res = new Node();
+    var curr = res;
+    while(l1 !== null && l2 !== null) {
+      if(l1.val <= l2.val) {
+        // Set current node to l1 if less than or equal
+        curr.next = l1;
+        // Move l1's head to next
+        l1 = l1.next;
+      } else {
+        // Else same case for l2
+        curr.next = l2;
+        l2 = l2.next;
+      }
+      // Move current to next
+      curr = curr.next;
+    }
+
+    if(l1 !== null) {
+      curr.next = l1;
+    } else if(l2 !== null) {
+      curr.next = l2;
+    }
+
+    return res.next;
   }
 
 }
