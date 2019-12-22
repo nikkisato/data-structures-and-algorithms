ab47fec4e5e3ef97a32e66c317c0ad0ccd784a8d
Nikki <nikki.satopdx@gmail.com>
2019-12-22 13:05:03 -0800

    got one test

---
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.test.js
@@ -0,0 +1,18 @@
+const { Queue } = require('../fifoAnimalShelter/fifo-animal-shelter');
+
+describe('Animal shelter queue', () => {
+  it('can add an animal node to the queue', () => {
+    const queue = new Queue();
+    queue.enqueue('dog');
+    expect(queue.front.value).toEqual('dog');
+    expect(queue.last.value).toEqual('dog');
+    queue.enqueue('cat');
+    expect(queue.front.value).toEqual('dog');
+    expect(queue.last.value).toEqual('cat');
+    queue.enqueue('dog');
+    expect(queue.front.value).toEqual('dog');
+    expect(queue.last.value).toEqual('dog');
+  });
+
+
+});
